9b32c6409f8e1ef49ebb9b74cdd40bada03d81ca
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 04:25:46 +0300

    Исправлено деление на 0Э

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,12 +2,19 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
+
+    if a == 0:
+        print("Not a quadratic equation")
+        return None, None
+
     discriminant = b ** 2 - 4 * a * c
     if discriminant == 0:
         root = -b / (2 * a)
         return root, None
+
     elif discriminant < 0:
         return None, None
+
     else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
