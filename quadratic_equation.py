8b2d8f6f3839ab22c13ed9d67d2e6917cb42f49a
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 03:18:31 +0300

    Исправлены имена функций

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,8 +4,8 @@ from math import sqrt
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant == 0:
-        root1 = -b / (2 * a)
-        return root1, None
+        root = -b / (2 * a)
+        return root, None
     elif discriminant < 0:
         return None, None
     else:
