51e71c6e7446531a8efcf5cc3294f2e648992b1f
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 16:08:10 +0300

    added function

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,21 +1,19 @@
 from math import sqrt
 
 
+def get_negative_root(a, b, D):
+    return (-b - sqrt(D)) / (2 * a)
+
+
 def get_roots(a, b, c):
-
-    if a == 0:
-        print("Not a quadratic equation")
-        return None, None
-
     discriminant = b ** 2 - 4 * a * c
-    if discriminant == 0:
-        root = -b / (2 * a)
-        return root, None
-
-    elif discriminant < 0:
+    if a == 0 | discriminant < 0:
         return None, None
+
+    if discriminant == 0:
+        return get_negative_root(a, b, discriminant), None
 
     else:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root1 = get_negative_root(a, b, discriminant)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
