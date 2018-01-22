53f422674824ba27196078897fb5655729b20099
Puzanira <puzanova.ira2011@yandex.ru>
2018-01-22 21:33:36 +0300

    Переделана функция

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,19 +1,13 @@
 from math import sqrt
 
 
-def get_negative_root(a, b, D):
-    return (-b - sqrt(D)) / (2 * a)
-
-
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if a == 0 | discriminant < 0:
         return None, None
-
+    root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
-        return get_negative_root(a, b, discriminant), None
-
+        return root1, None
     else:
-        root1 = get_negative_root(a, b, discriminant)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
